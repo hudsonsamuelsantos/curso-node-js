53fe54b2cdd74d25d374f23e89078b7062eb85e0
Hudson Santos <hudsnss@gmail.com>
2022-12-10 15:57:37 -0300

    explorando o console

---
+++ 2-fundamentos/7-explorando-o-console/index.js
@@ -0,0 +1,15 @@
+const x = 10
+const y = 'Hudson'
+const z = [1, 2]
+
+console.log(x, y, z)
+
+console.count(`o valor de x é: ${x}, contagem:`)
+console.count(`o valor de x é: ${x}, contagem:`)
+console.count(`o valor de x é: ${x}, contagem:`)
+
+console.log('O meu nome é %s', y)
+
+setTimeout(() => {
+    console.clear()
+}, 2000)
