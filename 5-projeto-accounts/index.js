9a87b8efe0e842e8f4f117ff2d5b89dafe8b73a0
Hudson Santos <hudsnss@gmail.com>
2022-12-14 06:02:33 -0300

    iniciando sistema

--- 5-projeto-accounts/index.js
+++ 5-projeto-accounts/index.js
@@ -2,4 +2,20 @@ const inquirer = require('inquirer')
 const chalk = require('chalk')
 const fs = require('fs')
 
-console.log('Iniciamos o Accounts!')
+operation()
+
+function operation() {
+    inquirer.prompt([
+        {
+            type: 'list',
+            name: 'action',
+            message: 'O que você deseja fazer?',
+            choices: ['Criar conta', 'Consultar saldo', 'Depositar', 'Sacar', 'Sair']
+        }
+    ]).then(answer => {
+        const action = answer['action']
+
+        console.log(action)
+    })
+        .catch(err => console.log(err))
+}
