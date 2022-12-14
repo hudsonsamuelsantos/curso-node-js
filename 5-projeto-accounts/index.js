d76061ffe3aaaf9e0ad781709dadfdc62f22de6e
Hudson Santos <hudsnss@gmail.com>
2022-12-14 06:08:15 -0300

    função inicial ao criar conta

--- 5-projeto-accounts/index.js
+++ 5-projeto-accounts/index.js
@@ -15,7 +15,14 @@ function operation() {
     ]).then(answer => {
         const action = answer['action']
 
-        console.log(action)
+        if (action === 'Criar conta') {
+            createAccount()
+        }
     })
         .catch(err => console.log(err))
+}
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
 }
