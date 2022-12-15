87bcb50d95ef7bd32f7b21fc6f664440c3bf5f02
Hudson Santos <hudsnss@gmail.com>
2022-12-14 22:13:40 -0300

    função de criar conta

--- 5-projeto-accounts/index.js
+++ 5-projeto-accounts/index.js
@@ -25,4 +25,39 @@ function operation() {
 function createAccount() {
     console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'))
     console.log(chalk.green('Defina as opções da sua conta a seguir'))
+
+    buildAccount()
+}
+
+function buildAccount() {
+    inquirer.prompt([
+        {
+            name: 'accountName',
+            message: 'Digite um nome para sua conta:'
+        }
+    ]).then(answer => {
+        const accountName = answer['accountName']
+
+        console.info(accountName)
+
+        if (!fs.existsSync('accounts')) {
+            fs.mkdirSync('accounts')
+        }
+
+        if (fs.existsSync(`accounts/${accountName}.json`)) {
+            console.log(chalk.bgRed.black('Esta conta já existe, escolha outro nome!'))
+
+            buildAccount()
+
+            return
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}', err => {
+            console.log(err)
+        })
+
+        console.log(chalk.green(`Parabéns, a sua conta de nome: ${accountName} foi criada com sucesso!`))
+
+        operation()
+    }).catch(err => console.log(err))
 }
