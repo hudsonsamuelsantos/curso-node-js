0569ba42c7277245b7e9b6de0ffaa2d8317f6192
Hudson Santos <hudsnss@gmail.com>
2022-12-17 12:12:45 -0300

    renderizando html com o express

---
+++ 6-express/3-render-html/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+const baseUrl = path.join(__dirname, 'templates')
+
+app.get('/', (req, res) => {
+    res.sendFile(`${baseUrl}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}!`)
+})
