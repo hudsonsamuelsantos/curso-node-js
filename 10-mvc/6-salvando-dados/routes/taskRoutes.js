08cd009b8d6f7cbe636099c7407510ebd10a448a
Hudson Santos <hudsnss@gmail.com>
2023-01-19 05:43:50 -0300

    salvando no banco utilizando arquitetura MVC

---
+++ 10-mvc/6-salvando-dados/routes/taskRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const router = express.Router()
+
+const taskController = require('../controllers/TaskController')
+
+router.get('/add', taskController.createTask)
+router.post('/add', taskController.createTaskSave)
+router.get('/', taskController.showTasks)
+
+module.exports = router
