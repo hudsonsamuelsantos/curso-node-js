09575a776f72886234cf67515f0138953576e3d3
Hudson Santos <hudsnss@gmail.com>
2023-02-09 21:05:45 -0300

    editando tarefa

---
+++ 10-mvc/9-editar-tarefa/routes/taskRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+
+const taskController = require('../controllers/TaskController')
+
+router.get('/add', taskController.createTask)
+router.post('/add', taskController.createTaskSave)
+router.post('/remove', taskController.removeTask)
+router.get('/edit/:id', taskController.updateTask)
+router.post('/edit', taskController.updateTaskPost)
+router.get('/', taskController.showTasks)
+
+module.exports = router
